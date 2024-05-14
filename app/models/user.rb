b50c883de3a7e4591a6cfa4295d442215055b2d6
ito-mirai <tetudounohi@gmail.com>
2024-05-14 19:57:52 +0900

    不要なメッセージの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,25 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  #↓正規表現で制約をかけているためおそらくいらない？
-  # validates :f_name, presence: true
-  # validates :p_name, presence: true
-  # validates :f_name_kana, presence: true
-  # validates :p_name_kana, presence: true
   validates :birthday, presence: true
 
     #全角かなカナ漢字に制約
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :f_name
     validates :p_name
   end
     #全角カナに制約
-  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
     validates :f_name_kana
     validates :p_name_kana
   end
-
     #passwordを英数字混合に制約
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX
 end
