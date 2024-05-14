524d8748554c61f942bf10b0575ec6888054897e
ito-mirai <tetudounohi@gmail.com>
2024-05-14 21:11:54 +0900

    Rubocopを実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-  #環境変数の設定
-  def basic_auth  
+  # 環境変数の設定
+  def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更済
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述に変更済
     end
   end
 
-  #deviseのUserモデルにパラメーターを許可
+  # deviseのUserモデルにパラメーターを許可
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :f_name, :p_name, :f_name_kana, :p_name_kana, :birthday])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,17 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
 
-    #全角かなカナ漢字に制約
+  # 全角かなカナ漢字に制約
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :f_name
     validates :p_name
   end
-    #全角カナに制約
+  # 全角カナに制約
   with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
     validates :f_name_kana
     validates :p_name_kana
   end
-    #passwordを英数字混合に制約
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  # passwordを英数字混合に制約
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX
 end
