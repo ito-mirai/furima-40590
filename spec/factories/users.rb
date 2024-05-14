78d4a77986a051508d126a63b82293b6e9000b51
ito-mirai <tetudounohi@gmail.com>
2024-05-14 20:58:26 +0900

    userモデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {Faker::Internet.username}
+    email                  {Faker::Internet.email}
+    password               {Faker::Internet.password}
+    password_confirmation  {password}
+    f_name                 {Gimei.last.kanji}
+    p_name                 {Gimei.first.kanji}
+    f_name_kana            {Gimei.last.katakana}
+    p_name_kana            {Gimei.first.katakana}
+    birthday               {Faker::Date.between(from: '1930-01-01', to: '2019-12-31')}
+  end
+end
