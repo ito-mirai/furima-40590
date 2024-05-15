3582f01f10ab5d6d9ce0ac8d61a6fb0c0139fa47
ito-mirai <tetudounohi@gmail.com>
2024-05-15 14:47:54 +0900

    アソシエーションとバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :quality_id, presence: true
+  validates :ship_load_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :ship_day_id, presence: true
+  validates :price, presence: true
+
 end
