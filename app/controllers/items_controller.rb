133551acb53590467595f716a3b15de7365c06b0
ito-mirai <tetudounohi@gmail.com>
2024-05-15 18:05:30 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    Item.new(item_params)
+  end
+
+  def create
+  end
+
+  private
+
+  def item_params
+    params.require(
+            :item
+          ).
+          permit(
+            :name, :description, :category_id, :quality_id, :ship_load_id, :prefecture_id, :ship_day_id, :price 
+          ).
+          merge(
+            user_id: current_user.id
+          )
+  end
+
 end
