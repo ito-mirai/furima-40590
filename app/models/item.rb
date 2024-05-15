e37568a850159fc00648b7f4a9529c972bf79335
ito-mirai <tetudounohi@gmail.com>
2024-05-15 17:12:12 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,20 @@
 class Item < ApplicationRecord
 
   belongs_to :user
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :ship_load
+  belongs_to :prefecture
+  belongs_to :ship_day
   has_one_attached :image
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :quality_id, presence: true
-  validates :ship_load_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :ship_day_id, presence: true
+  validates :category, numericality: { other_than: 1 } 
+  validates :quality, numericality: { other_than: 1 } 
+  validates :ship_load, numericality: { other_than: 1 } 
+  validates :prefecture, numericality: { other_than: 1 } 
+  validates :ship_day, numericality: { other_than: 1 } 
   validates :price, presence: true
 
 end
