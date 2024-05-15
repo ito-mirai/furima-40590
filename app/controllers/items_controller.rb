e60a13182769dee91dd84573dd563f9aee6207a7
ito-mirai <tetudounohi@gmail.com>
2024-05-15 20:31:38 +0900

    ログイン状態に基づくページ遷移の分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
 
+  #ログインしていないとき、ログインページへ遷移する
+  before_action :move_to_sign_in, only: :new
+
   def index
   end
 
@@ -8,22 +11,27 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def item_params
-    params.require(
-            :item
-          ).
-          permit(
-            :image, :name, :description, :category_id, :quality_id, :ship_load_id, :prefecture_id, :ship_day_id, :price 
-          ).
-          merge(
-            user_id: current_user.id
-          )
+    params
+      .require(:item)
+      .permit(:image, :name, :description, :category_id, :quality_id, :ship_load_id, :prefecture_id, :ship_day_id, :price )
+      .merge(user_id: current_user.id)
+  end
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
 end
