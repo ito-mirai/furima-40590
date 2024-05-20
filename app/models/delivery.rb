f25e77e423038ac03c864d2fced5a64bf746244f
ito-mirai <tetudounohi@gmail.com>
2024-05-20 18:27:15 +0900

    purchasesテーブルとdeliveriesテーブルの作成と各モデルの記述

---
+++ app/models/delivery.rb
@@ -0,0 +1,12 @@
+class Delivery < ApplicationRecord
+  belongs_to :purchase
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  validates :post_code, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :municipality, presence: true
+  validates :address, presence: true
+  validates :telephone, presence: true
+end
