59d7f1f962942cf904012a8ec769d38fdbde6cf3
ito-mirai <tetudounohi@gmail.com>
2024-05-21 19:25:33 +0900

    正規表現の追加とコメントアウトの削除

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -4,9 +4,4 @@ class Delivery < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 
-  # validates :post_code, presence: true
-  # validates :prefecture_id, numericality: { other_than: 1 } 
-  # validates :municipality, presence: true
-  # validates :address, presence: true
-  # validates :telephone, presence: true
 end
--- app/models/purchase_delivery.rb
+++ app/models/purchase_delivery.rb
@@ -2,20 +2,15 @@ class PurchaseDelivery
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building, :telephone, :purchase_id
   
-  #バリデーションを記述
   with_options presence: do
     validates :user_id
     validates :item_id
-    validates :post_code
+    validates :post_code, format: /\A[0-9]{3}-[0-9]{4}\z/
     validates :prefecture_id, numericality: { other_than: 1 } 
     validates :municipality
     validates :address
-    validates :telephone
-    # validates :purchase_id
+    validates :telephone, format: /\A[0-9]{10,11}\z/
   end
-  
-  #なおアソシエーションを定義することは出来ないため、belongs_toによるバリデーションは行えない
-  #そのため普通のバリデーションとして記述する必要がある
   
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
