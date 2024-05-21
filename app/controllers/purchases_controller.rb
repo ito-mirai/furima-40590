d4f30b62a7c9263f52e575e21029ffbf32976ba1
ito-mirai <tetudounohi@gmail.com>
2024-05-21 18:41:31 +0900

    商品購入ページへの遷移設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,12 @@
 class PurchasesController < ApplicationController
-
+  # ＠itemに対して特定のitemレコードを代入
   before_action :item_find
+
+  # ログインしていないとき、ログインページへ遷移する
+  before_action :move_to_sign_in
+
+  # 購入不可のとき、トップページへ遷移する
+  before_action :move_to_index
 
   def index
     @purchase_delivery = PurchaseDelivery.new
@@ -10,6 +16,7 @@ class PurchasesController < ApplicationController
     @purchase_delivery = PurchaseDelivery.new(purchase_delivery_params)
       if @purchase_delivery.valid?
         @purchase_delivery.save
+        redirect_to root_path
       else
         render :index, status: :unprocessable_entity
       end
@@ -26,6 +33,18 @@ class PurchasesController < ApplicationController
 
   def item_find
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_sign_in
+    return if user_signed_in?
+
+    redirect_to new_user_session_path
+  end
+
+  def move_to_index
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
 end
