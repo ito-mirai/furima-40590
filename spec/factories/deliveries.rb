e0211fd4e22318fb4024915edab6aaabdfd639ab
ito-mirai <tetudounohi@gmail.com>
2024-05-23 14:10:32 +0900

    formオブジェクトのテストコードを記述

--- spec/factories/deliveries.rb
+++ spec/factories/deliveries.rb
@@ -1,4 +1,11 @@
 FactoryBot.define do
   factory :delivery do
+    # post_code     { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
+    # prefecture_id { Faker::Number.within(range: 2..48) }
+    # municipality  { Gimei.city.kanji }
+    # address       { "#{Faker::Number.digit}-#{Faker::Number.within(range: 1..50)}" }
+    # building      { Faker::Address.secondary_address }
+    # telephone     { "#{Faker::Number.within(range: 1000000000..99999999999)}" }
+    # association   :purchase
   end
 end
--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :purchase do
+    # token         {"tok_abcdefghijk00000000000000000"}
+    # association   :user
+    # association   :item
   end
 end
